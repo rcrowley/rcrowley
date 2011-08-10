c23f6ba6542f54a63686d492de947e54e2b22aa9
Richard Crowley <r@rcrowley.org>
2011-08-10 13:43:02 -0700

    Use ANSI red in PS1 prompt.

--- .profile.d/prompt.sh
+++ .profile.d/prompt.sh
@@ -1,14 +1,14 @@
 # Release and architecture prompt.
 PS1="$(
 	lsb_release -c 2>/dev/null | cut -f2
-)\[\e[1m\]-\[\e[0m\]$(
+)\[\e[1;31m\]-\[\e[0m\]$(
 	dpkg --print-architecture 2>/dev/null
-) \h\[\e[1m\]:\[\e[0m\]\w\n\[\e[1m\]$(
+) \h\[\e[1;31m\]:\[\e[0m\]\w\n\[\e[1;31m\]$(
 	[ "$(whoami)" = "root" ] && echo "#" || echo "\$"
 )\[\e[0m\] "
 
 # Git branch in prompt.
 [ -f /etc/bash_completion.d/git ] && {
 	. /etc/bash_completion.d/git
-	[ "\$(type -t __git_ps1)" ] && PS1="\$(__git_ps1 '\[\e[1m\](\[\e[0m\]%s\[\e[1m\])\[\e[0m\] ')$PS1"
+	[ "\$(type -t __git_ps1)" ] && PS1="\$(__git_ps1 '\[\e[1;31m\](\[\e[0m\]%s\[\e[1;31m\])\[\e[0m\] ')$PS1"
 }
