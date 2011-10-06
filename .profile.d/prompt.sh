fe6a318a1e8d2ea3746d8415c1ea0797b870c9b7
Richard Crowley <r@rcrowley.org>
2011-10-06 05:19:32 +0000

    Reworked prompt.

--- .profile.d/prompt.sh
+++ .profile.d/prompt.sh
@@ -1,5 +1,6 @@
-# Release and architecture prompt.
-PS1="$(
+[ -f "/etc/bash_completion.d/git" ] && . "/etc/bash_completion.d/git"
+
+PS1="\[\e[1;31m\]█\[\e[0m\] $(
 	lsb_release -c 2>/dev/null | cut -f2
 )\[\e[1;31m\]-\[\e[0m\]$(
 	dpkg --print-architecture 2>/dev/null
@@ -8,12 +9,8 @@ PS1="$(
 		echo -n "\[\e[1;31m\]/\[\e[0m\]"
 		cat "/.lxcme"
 	}
-)\[\e[1;31m\]:\[\e[0m\]\w\n\[\e[1;31m\]$(
+)\[\e[1;31m\]:\[\e[0m\]\w$(
+	[ "\$(type -t __git_ps1)" ] && echo -n "\$(__git_ps1 ' \[\e[1;31m\](\[\e[0m\]%s\[\e[1;31m\])\[\e[0m\]')"
+)\n\[\e[1;31m\]█\[\e[0m\] \[\e[1;31m\]$(
 	[ "$(whoami)" = "root" ] && echo "#" || echo "\$"
 )\[\e[0m\] "
-
-# Git branch in prompt.
-[ -f /etc/bash_completion.d/git ] && {
-	. /etc/bash_completion.d/git
-	[ "\$(type -t __git_ps1)" ] && PS1="\$(__git_ps1 '\[\e[1;31m\](\[\e[0m\]%s\[\e[1;31m\])\[\e[0m\] ')$PS1"
-}
