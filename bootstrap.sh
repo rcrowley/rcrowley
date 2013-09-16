8649bf62a4d61fa02aa7094eb773f8459d701253
Richard Crowley <r@rcrowley.org>
2013-09-16 14:38:56 +0000

    Complete the backup and install www for real.

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,15 @@ ssh "rcrowley.org" gpg --armor --export-secret-key "packages@rcrowley.org" | gpg
 ssh "rcrowley.org" gpg --armor --export --secret "r@rcrowley.org" | gpg --import
 ssh "rcrowley.org" gpg --armor --export-secret-key "r@rcrowley.org" | gpg --import || :
 
-mkdir -p "var/cache/freight"
+mkdir -p "src" "var/cache/freight" "var/lib" "var/www"
 curl -o"var/cache/freight/keyring.gpg" -s "http://packages.rcrowley.org/keyring.gpg"
 curl -o"var/cache/freight/pubkey.gpg" -s "http://packages.rcrowley.org/pubkey.gpg"
-mkdir -p "var/lib"
+rsync -av "rcrowley.org":"src/git.squareup.com" "src"
+rsync -av "rcrowley.org":"var/backup" "var"
+rsync -av "rcrowley.org":"var/lib/Papers2" "var/lib"
 rsync -Hav "rcrowley.org":"var/lib/freight" "var/lib"
+rsync -av "rcrowley.org":"var/lib/git" "var/lib"
+rsync -av "rcrowley.org":"var/www/work" "var/www"
 
 cat >"/etc/apt/sources.list.d/rcrowley.list" <<EOF
 deb http://packages.rcrowley.org $(lsb_release -sc) main
@@ -28,28 +32,14 @@ apt-get -y install "freight" "gnupg-agent" "pinentry-curses" "ruby" "rubygems"
 apt-get -y remove "pinentry-gtk2"
 which "fpm" || gem install --no-rdoc --no-ri "fpm"
 
-if [ ! -f "go$VERSION.linux-amd64.tar.gz" ]
-then curl -O "http://go.googlecode.com/files/go$VERSION.linux-amd64.tar.gz"
-fi
-if [ ! -f "go_${VERSION}-${BUILD}_amd64.deb" ]
+if [ ! -f "var/lib/freight/apt/$(lsb_release -sc)/go_${VERSION}-${BUILD}_amd64.deb" ]
 then
-    trap "rm -rf \"go\"" EXIT INT QUIT TERM
+    curl -O "http://go.googlecode.com/files/go$VERSION.linux-amd64.tar.gz"
+    trap "rm -rf \"go\" \"go$VERSION.linux-amd64.tar.gz\"" EXIT INT QUIT TERM
     tar xf "go$VERSION.linux-amd64.tar.gz"
-    fakeroot fpm -m"Richard Crowley <r@rcrowley.org>" -n"go" -p"go_${VERSION}-${BUILD}_amd64.deb" --prefix="/usr" -s"dir" -t"deb" -v"$VERSION-$BUILD" "go"
-    rm -rf "go"
-    freight add "go_${VERSION}-${BUILD}_amd64.deb" "apt/$(lsb_release -sc)"
+    fakeroot fpm -m"Richard Crowley <r@rcrowley.org>" -n"go" -p"var/lib/freight/apt/$(lsb_release -sc)/go_${VERSION}-${BUILD}_amd64.deb" --prefix="/usr" -s"dir" -t"deb" -v"$VERSION-$BUILD" "go"
+    rm -rf "go" "go$VERSION.linux-amd64.tar.gz"
 fi
-
-if [ -f ".gpg-agent-info" ]
-then . "./.gpg-agent-info"
-fi
-if ! gpg-agent
-then gpg-agent --daemon --default-cache-ttl 315360000 --max-cache-ttl 315360000 --write-env-file ".gpg-agent-info"
-fi
-sleep 1
-. "./.gpg-agent-info"
-export GPG_AGENT_INFO
-freight cache
 
 apt-get -y install "git"
 if [ ! -d ".git" ]
@@ -60,10 +50,26 @@ fi
 git remote update "origin"
 git fetch "origin"
 if [ ! -f ".git/refs/heads/master" ]
-then rm -f ".profile"
+then rm -f ".profile" "bootstrap.sh"
 fi
 git merge "origin/master"
 
+if [ -f ".gpg-agent-info" ]
+then
+    . "./.gpg-agent-info"
+    export GPG_AGENT_INFO
+fi
+if ! gpg-agent
+then gpg-agent --daemon --default-cache-ttl 315360000 --max-cache-ttl 315360000 --write-env-file ".gpg-agent-info"
+fi
+sleep 1
+. "./.gpg-agent-info"
+export GPG_AGENT_INFO
+export GPG_TTY="$(tty)"
+freight cache
+apt-get update
+
+apt-get install "go"
 mkdir -p "src/github.com/rcrowley"
 if [ ! -d "src/github.com/rcrowley/go-metrics" ]
 then git clone "git@github.com:rcrowley/go-metrics.git" "src/github.com/rcrowley/go-metrics"
@@ -72,6 +78,8 @@ if [ ! -d "src/github.com/rcrowley/go-tigertonic" ]
 then git clone "git@github.com:rcrowley/go-tigertonic.git" "src/github.com/rcrowley/go-tigertonic"
 fi
 
+. ".profile.d/go.sh"
+go install "www"
 cat >"/etc/init/www.conf" <<EOF
 description "www"
 start on runlevel [2345]
@@ -80,7 +88,7 @@ respawn
 chdir /root
 env GOPATH="/root"
 env GOROOT="/usr/go"
-env PATH="/root/bin:/usr/go/bin:/usr/bin:/bin"
+env PATH="/root/bin:/usr/bin:/bin:/usr/go/bin"
 script
     set -e
     rm -f "/tmp/www.log"
@@ -91,4 +99,4 @@ script
     exec /root/bin/www
 end script
 EOF
-start "www"
+start "www" || restart "www"
