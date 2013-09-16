1b7092d36d943d2ed15d44fd18651e80322f067f
Richard Crowley <r@rcrowley.org>
2013-09-16 15:33:45 +0000

    Seemingly final tweaks.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,23 +3,36 @@ BUILD="rcrowley1"
 
 set -e -x
 
-scp -o StrictHostKeyChecking="no" "rcrowley.org":".ssh/authorized_keys" ".ssh"
+# Add known hosts entries to make this program more headless.
+mkdir -m"700" -p ".ssh"
+if ! grep -q "github.com" ".ssh/known_hosts"
+then ssh-keyscan "github.com" >>".ssh/known_hosts"
+fi
+if ! grep -q "rcrowley.org" ".ssh/known_hosts"
+then ssh-keyscan "rcrowley.org" >>".ssh/known_hosts"
+fi
 
+# Copy authorized SSH public keys from the old rcrowley.org.
+scp "rcrowley.org":".ssh/authorized_keys" ".ssh"
+
+# Copy GPG key pairs from the old rcrowley.org.
 ssh "rcrowley.org" gpg --armor --export --secret "packages@rcrowley.org" | gpg --import
 ssh "rcrowley.org" gpg --armor --export-secret-key "packages@rcrowley.org" | gpg --import || :
 ssh "rcrowley.org" gpg --armor --export --secret "r@rcrowley.org" | gpg --import
 ssh "rcrowley.org" gpg --armor --export-secret-key "r@rcrowley.org" | gpg --import || :
 
+# Copy files from the old rcrowley.org.
 mkdir -p "src" "var/cache/freight" "var/lib" "var/www"
 curl -o"var/cache/freight/keyring.gpg" -s "http://packages.rcrowley.org/keyring.gpg"
 curl -o"var/cache/freight/pubkey.gpg" -s "http://packages.rcrowley.org/pubkey.gpg"
 rsync -av "rcrowley.org":"src/git.squareup.com" "src"
-rsync -av "rcrowley.org":"var/backup" "var"
+rsync -av "rcrowley.org":"var/backups" "var"
 rsync -av "rcrowley.org":"var/lib/Papers2" "var/lib"
 rsync -Hav "rcrowley.org":"var/lib/freight" "var/lib"
 rsync -av "rcrowley.org":"var/lib/git" "var/lib"
 rsync -av "rcrowley.org":"var/www/work" "var/www"
 
+# Add our Debian archive to APT.
 cat >"/etc/apt/sources.list.d/rcrowley.list" <<EOF
 deb http://packages.rcrowley.org $(lsb_release -sc) main
 #deb-src http://packages.rcrowley.org $(lsb_release -sc) main
@@ -28,10 +41,12 @@ cp "var/cache/freight/keyring.gpg" "/etc/apt/trusted.gpg.d/rcrowley.gpg"
 apt-get update
 apt-get -y upgrade
 
+# Install dependencies.
 apt-get -y install "freight" "gnupg-agent" "pinentry-curses" "ruby" "rubygems"
 apt-get -y remove "pinentry-gtk2"
 which "fpm" || gem install --no-rdoc --no-ri "fpm"
 
+# Build the Debian package for Go if it doesn't already exist.
 if [ ! -f "var/lib/freight/apt/$(lsb_release -sc)/go_${VERSION}-${BUILD}_amd64.deb" ]
 then
     curl -O "http://go.googlecode.com/files/go$VERSION.linux-amd64.tar.gz"
@@ -41,6 +56,7 @@ then
     rm -rf "go" "go$VERSION.linux-amd64.tar.gz"
 fi
 
+# Clone the home directory.
 apt-get -y install "git"
 if [ ! -d ".git" ]
 then
@@ -54,6 +70,7 @@ then rm -f ".profile" "bootstrap.sh"
 fi
 git merge "origin/master"
 
+# Start a GPG agent and cache the Debian archive.
 if [ -f ".gpg-agent-info" ]
 then
     . "./.gpg-agent-info"
@@ -69,6 +86,7 @@ export GPG_TTY="$(tty)"
 freight cache
 apt-get update
 
+# Install Go and clone dependencies.
 apt-get install "go"
 mkdir -p "src/github.com/rcrowley"
 if [ ! -d "src/github.com/rcrowley/go-metrics" ]
@@ -78,6 +96,7 @@ if [ ! -d "src/github.com/rcrowley/go-tigertonic" ]
 then git clone "git@github.com:rcrowley/go-tigertonic.git" "src/github.com/rcrowley/go-tigertonic"
 fi
 
+# Build, install, and start www.
 . ".profile.d/go.sh"
 go install "www"
 cat >"/etc/init/www.conf" <<EOF
@@ -100,3 +119,7 @@ script
 end script
 EOF
 start "www" || restart "www"
+
+# Remove the old rcrowley.org's SSH host key.
+grep -v "rcrowley.org" ".ssh/known_hosts" >"known_hosts"
+mv "known_hosts" ".ssh"
