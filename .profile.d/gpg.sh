2493321cae1fe0037adbaea4ebf1b3e18b1a5922
Richard Crowley <r@rcrowley.org>
2014-01-08 18:54:47 +0000

    Always have a GPG agent around.

--- .profile.d/gpg.sh
+++ .profile.d/gpg.sh
@@ -1,1 +1,10 @@
 export GPG_TTY="$(tty)"
+
+if [ -f "$HOME/.gpg-agent-info" ]
+then
+    . "$HOME/.gpg-agent-info"
+    export GPG_AGENT_INFO
+fi
+if ! gpg-connect-agent "/bye"
+then eval "$(gpg-agent --daemon --disable-scdaemon --write-env-file "$HOME/.gpg-agent-info")"
+fi
