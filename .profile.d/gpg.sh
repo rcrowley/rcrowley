79f6218c697ccc327a988c181b829069a1e06ca0
Richard Crowley <r@rcrowley.org>
2014-03-30 09:43:08 -0700

    Don't bother with gpg-connect-agent because it's not there on Mac.

--- .profile.d/gpg.sh
+++ .profile.d/gpg.sh
@@ -5,6 +5,6 @@ then
     . "$HOME/.gpg-agent-info"
     export GPG_AGENT_INFO
 fi
-if ! gpg-connect-agent "/bye"
+if ! gpg-agent
 then eval "$(gpg-agent --daemon --disable-scdaemon --write-env-file "$HOME/.gpg-agent-info")"
 fi
