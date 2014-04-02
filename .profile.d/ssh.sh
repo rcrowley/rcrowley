3faf355e6930b27af50540753f8ee3e4326be099
Richard Crowley <r@rcrowley.org>
2014-04-02 09:08:41 -0700

    Faster terminal starts on Mac.

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -15,8 +15,8 @@ then eval "$(ssh-agent -t"600")"
 fi
 
 # After an ssh-agent is running, pull the latest home directory from GitHub.
-# Don't bother doing this over and over once tmux is running.
-if [ -z "$TMUX" -a "$HOME" = "$PWD" ]
+# Don't bother doing this over and over on the Mac or once tmux is running.
+if ! which "sw_vers" >"/dev/null" 2>"/dev/null" && [ -z "$TMUX" -a "$HOME" = "$PWD" ]
 then
     git remote update origin
     git pull origin master
