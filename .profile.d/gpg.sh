db35e4e9d6c35aaa456be4c973ac4d7548ab09fc
Richard Crowley <r@rcrowley.org>
2015-06-13 07:40:27 -0700

    Don't print errors from gpg-agent being missing

--- .profile.d/gpg.sh
+++ .profile.d/gpg.sh
@@ -5,6 +5,6 @@ then
     . "$HOME/.gpg-agent-info"
     export GPG_AGENT_INFO
 fi
-if ! gpg-agent
+if which "gpg-agent" >"/dev/null" 2>"/dev/null" && ! gpg-agent
 then eval "$(gpg-agent --daemon --disable-scdaemon --write-env-file "$HOME/.gpg-agent-info")"
 fi
