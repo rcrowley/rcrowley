741e03d5ae6d1b07a8fadfd01f3360dd7e9c2b99
Richard Crowley <rcrowley@slack-corp.com>
2016-09-12 13:11:54 -0700

    New Go tool locations

--- .profile.d/go.sh
+++ .profile.d/go.sh
@@ -1,1 +1,6 @@
-export GOPATH="$HOME" GOROOT="/usr/local/go" PATH="$PATH:/usr/local/go/bin"
+if [ -d "/usr/local/go" ]
+then export GOROOT="/usr/local/go" PATH="/usr/local/go/bin:$PATH"
+elif [ -d "/usr/go" ]
+then export GOROOT="/usr/go" PATH="/usr/go/bin:$PATH"
+fi
+export GOPATH="$HOME"
